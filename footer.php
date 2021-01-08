c352e10b8e3565bbdadf3eeec5ce113435b7646f
karl-hin <karl.hinterseber@le-campus-numerique.fr>
2021-01-08 16:41:41 +0100

    ajout page php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>© 2020 </p>
+</footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Karl Hinterseber</title>
+    <link href="styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+<header>
+    <div id="entete">
+        <p class="alone">Passionné par les nouvelles technologies et l'informatique</p>
+        <img src="image/copie.jpg" id="avatar_karl" width="100" height="111" alt="photo Karl Hinterseber" id="photo-profile" />
+    </div>
+    <div class="boutonnav" id="navmain">
+        <a class="active" href="./index.html">Présentation</a>
+        <a class="boutonsnav" href="./hobbies.html">Hobbies</a>
+        <a class="boutonsnav" href="./contacts.html">Contact</a>
+    </div>
+</header>
